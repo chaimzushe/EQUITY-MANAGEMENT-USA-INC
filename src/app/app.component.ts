4a975ebb658739ef987e0f9a6bea893c70b464cf
Chaim Wilmowsky <chaimzushe@gmail.com>
2023-09-05 09:21:06 -0400

    add abe tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import LogRocket from 'logrocket';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'equity-management-site';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    const user =  {
+      name: 'Abe katz',
+      email: 'abekatz@gmail.vom'
+    }
+    LogRocket.init('mt2awk/say-kaddish');
+    LogRocket.identify('equity-management', {
+      release: 'b620fm',
+      name: user.name,
+      email: user.email,
+    });
+  }
+
 }
