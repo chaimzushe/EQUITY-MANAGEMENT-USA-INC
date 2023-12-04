2d397fb6a0dd45fc7967981009e25031f28bbd7b
Chaim Wilmowsky <chaimzushe@gmail.com>
2023-12-04 12:11:55 -0500

    remove logging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import LogRocket from 'logrocket';
+// import LogRocket from 'logrocket';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,16 +7,16 @@ import LogRocket from 'logrocket';
 })
 export class AppComponent implements OnInit{
   ngOnInit(): void {
-    const user =  {
-      name: 'Abe katz',
-      email: 'abekatz@gmail.vom'
-    }
-    LogRocket.init('mt2awk/say-kaddish');
-    LogRocket.identify('equity-management', {
-      release: 'b620fm',
-      name: user.name,
-      email: user.email,
-    });
+  //   const user =  {
+  //     name: 'Abe katz',
+  //     email: 'abekatz@gmail.vom'
+  //   }
+  //   LogRocket.init('mt2awk/say-kaddish');
+  //   LogRocket.identify('equity-management', {
+  //     release: 'b620fm',
+  //     name: user.name,
+  //     email: user.email,
+  //   });
   }
 
 }
